567185f88381a8c0def926c2cf044c2a6e17420a
zebaten <zebatenx@gmail.com>
2022-01-10 01:09:22 -0300

    server y readme

--- readme.txt
+++ readme.txt
@@ -2,8 +2,17 @@ Alejandra Araya
 Sebastián Navarrete
 -----------------------------------------
 
+-Se encuentra creardo el archivo MAKEFILE para poder compilar todo sólo ingresando el comando make en la consola
+dentro del directorio del proyecto.
 -La compilación del proyecto debe ser en un ambiente Linux (probado en y funcional en Ubuntu) y en modo SuperUser.
 -En orden de una correcta ejecución, abrir el archivo server, definir la cantidad de jugadores, abrir 
 la cantidad de clientes correspondiente a la cantidad de jugadores para que cada uno maneje e identificar
-en número de jugador correspondiente.
+el número de jugador correspondiente.
 una consola.
+-Al notar que un jugador envió su coordenada después que el otro jugador, el servidor responderá con el mensaje
+"muy lento".
+
+Comentarios:
+-En el archivo client.c, desde la línea 60 a la 75 se hacen los envíos de datos ingresados hacia el sevidor.
+-En en archivo server.c, desde la línea 169 se definen las tuberías, para luego de la 184 , abrirlas, por lo que si estas se encuentran
+abiertas, en la línea 37 a 41 se cerrarán en caso de volver a querer utilizarlas.
--- server.c
+++ server.c
@@ -28,13 +28,11 @@ int numeros[max_size];
 
 sem_t semaforos;
 
-void run1 (int jug1, int jug2, int t1, int t2, int g, int *x, int *y, int *j);
+void run1 (int jug1, int jug2, int t1, int t2, int g, int *x, int *y, int *jugad);
+void run2 (int jug1, int jug2,int jug3, int t1, int t2, int t3, int g);
+void run3 (int jug1, int jug2,int jug3,int jug4, int t1, int t2, int t3,int t4, int g);
 
-void run2 (int jug1, int jug2, int jug3, int t1, int t2, int t3, int g, int *x, int *y, int *j);
-
-void run3 (int jug1, int jug2, int jug3, int jug4, int t1, int t2, int t3, int t4, int g, int *x, int *y, int *j);
-
-void main (){
+int main (){
 
     unlink(FIFONAME);
     unlink(FIFONAME_JUG1);
@@ -43,10 +41,9 @@ void main (){
     unlink(FIFONAME_JUG4);
 
     int jugadores;
-    int i;
     int tamTabla;
     int num;
-    int x=0, y=0, j=0;
+    int x=0, y=0, jugad=0;
 
     printf("Cuantos jugadores? ");
     scanf("%d", &jugadores);
@@ -62,18 +59,7 @@ void main (){
         case 2:
             tamTabla=8;
             num = 16;
-            int tablero[tamTabla][tamTabla];
-            int valores[30];
-
-            int numeros = malloc(num * sizeof(int));
-            for(int i = 0; i < num; i++){
-                if(numeros[i] != numeros[i-1] && i > 0){
-                    numeros[i] = rand() %max_size +1;
-                }else if(i==0){
-                    numeros[i] = rand() %max_size +1;
-                }
-                
-            }
+            
             /*
             for(i=0; i<tamTabla; i++){
                 for (j = 0; j < tamTabla; j++)
@@ -90,23 +76,12 @@ void main (){
                 }
                 
             }*/
-            printTablero(tablero,tamTabla);
+            //printTablero(tablero,tamTabla);
             break;
         case 3:
             tamTabla=10;
             num = 25;
-            int tablero[tamTabla][tamTabla];
-            int valores[30];
-
-            int numeros = malloc(num * sizeof(int));
-            for(int i = 0; i < num; i++){
-                if(numeros[i] != numeros[i-1] && i > 0){
-                    numeros[i] = rand() %max_size +1;
-                }else if(i==0){
-                    numeros[i] = rand() %max_size +1;
-                }
-                
-            }
+            
             /*
             for(int i=0; i<tamTabla; i++){
                 for (int j = 0; j < tamTabla; j++)
@@ -122,24 +97,12 @@ void main (){
                 }
                 
             }*/
-            printTablero(tablero,tamTabla);
+            //printTablero(tablero,tamTabla);
             break;
 
         case 4:
             tamTabla=12;
             num = 36;
-            int tablero[tamTabla][tamTabla];
-            int valores[30];
-
-            int numeros = malloc(num * sizeof(int));
-            for(int i = 0; i < num; i++){
-                if(numeros[i] != numeros[i-1] && i > 0){
-                    numeros[i] = rand() %max_size +1;
-                }else if(i==0){
-                    numeros[i] = rand() %max_size +1;
-                }
-                
-            }
             /*
             for(int i=0; i<tamTabla; i++){
                 for (int j = 0; j < tamTabla; j++)
@@ -157,12 +120,48 @@ void main (){
                 
             }*/
 
-            printTablero(tablero,tamTabla);
+            //printTablero(tablero,tamTabla);
             break;
 
             default: break;
     }
 
+    int tablero[tamTabla][tamTabla];
+    int valores[50];
+    int i,j;
+    
+
+    for(i= 0; i<tamTabla;i++){
+        for(j=0;j<tamTabla;j++){
+            tablero[i][j] = 0;
+        }
+    }
+    for (i = 0; i < 50; i++){
+        valores[i] = i + 1;
+    }
+    for (i = 0; i < 50; i++){
+        int temporal = valores[i];
+        int random = rand () % 51;
+        valores[i] = valores[random];
+        valores[random] = temporal;
+    }
+    int rx, ry;
+    for (i = 0; i < num; i++){
+        do{
+            rx = rand () % (tamTabla + 1);
+            ry = rand () % (tamTabla + 1);
+        }while(tablero[rx][ry]!=0);
+        tablero[rx][ry]=valores[i];
+    }
+    //int numeros = malloc(num * sizeof(int));
+    /*for(int i = 0; i < 50; i++){
+        if(valores[i] != valores[i-1] && i > 0){
+            valores[i] = rand() %max_size +1;
+        }else if(i==0){
+            valores[i] = rand() %max_size +1;
+        }
+    }
+    */
 
     int jug1, jug2, jug3, jug4;
     int fd_g, fd_t1, fd_t2, fd_t3, fd_t4;
@@ -182,15 +181,15 @@ void main (){
         }
 
 
-        if(fd_t1 = open(FIFONAME_JUG1, O_RDWR)) < 0) { 
+        if((fd_t1 = open(FIFONAME_JUG1, O_RDWR)) < 0) { 
             perror("Open t1");
             exit(1);
         }
-        if(fd_t2 = open(FIFONAME_JUG2, O_RDWR)) < 0) { 
+        if((fd_t2 = open(FIFONAME_JUG2, O_RDWR)) < 0) { 
             perror("Open t2");
             exit(1);
         }
-        if(fd_g = open(FIFONAME, O_RDWR)) < 0) { 
+        if((fd_g = open(FIFONAME, O_RDWR)) < 0) { 
             perror("Open FDG");
             exit(1);
         }
@@ -212,7 +211,7 @@ void main (){
             perror("mkfifo");
             return(1);
         }
-        if(fd_t3 = open(FIFONAME_JUG3, O_RDWR)) < 0) { 
+        if((fd_t3 = open(FIFONAME_JUG3, O_RDWR)) < 0) { 
             perror("Open t3");
             exit(1);
         }
@@ -229,7 +228,7 @@ void main (){
             perror("mkfifo");
             return(1);
         }
-        if(fd_t3 = open(FIFONAME_JUG4, O_RDWR)) < 0) { 
+        if((fd_t3 = open(FIFONAME_JUG4, O_RDWR)) < 0) { 
             perror("Open t4");
             exit(1);
         }
@@ -248,7 +247,7 @@ void main (){
         else{
             while(1){
                 run1(jug1,jug2,fd_t1,fd_t2,fd_g, &x,&y,&j);
-                printf("coordenada x: %d y:%d jugador:%d",x,y,j);
+                printf("coordenada x: %d y:%d jugador:%d",x,y,jugad);
             }
         }
     }
@@ -257,21 +256,21 @@ void main (){
             while(1);
         }
         else{
-            run2(jug1,jug2,jug3,fd_t1,fd_t2,fd_t3,fd_g, &x,&y,&j);
+            run2(jug1,jug2,jug3,fd_t1,fd_t2,fd_t3,fd_g);
         }
     }
     if(jugadores == 4){ 
-        if(jug1 > 0 && jug >0){ 
+        if(jug1 > 0 && jug2 >0 && jug3 && jug4){ 
             while(1);
         }
         else{
-            run3(jug1,jug2,jug3,jug4,fd_t1,fd_t2,fd_t3,fd_t4,fd_g, &x,&y,&j);
+            run3(jug1,jug2,jug3,jug4,fd_t1,fd_t2,fd_t3,fd_t4,fd_g);
         }
     }
     return 0;
 }
 
-void printTablero(int **tablero, int Max){
+/*void printTablero(int **tablero, int Max){
     
     for(int i=0; i<Max;i++){
         for (int j = 0; j < Max; j++)
@@ -282,9 +281,9 @@ void printTablero(int **tablero, int Max){
         printf("\n");     
     }
 
-}
+}*/
 
-void run1 (int jug1, int jug2, int t1, int t2, int g, int *x, int *y, int *j){
+void run1 (int jug1, int jug2, int t1, int t2, int g, int *x, int *y, int *jugad){
     printf("2 jugadores\n");
     int a;
     char buffer[1024];
@@ -292,43 +291,43 @@ void run1 (int jug1, int jug2, int t1, int t2, int g, int *x, int *y, int *j){
     t2 = open(FIFONAME_JUG2, O_RDWR);
     if((a = read(g,buffer, sizeof(buffer))) > 0){
         write(1, buffer, a);
-        x* = buffer[0] - "0";
+        *x = buffer[0] - '0';
         close(g);
     }
     g = open(FIFONAME, O_RDWR);
     if((a = read(g,buffer, sizeof(buffer))) > 0){
         write(1, buffer, a);
-        y* = buffer[0] - "0";
+        *y = buffer[0] - '0';
         close(g);
     }
     if(jug1 == 0){
         write(t1, "muy lento", 21);
         close(t1);
-        *j=2;
+        *jugad=2;
     }
     else{
         write(t2, "muy lento", 21);
         close(t2);
-        *j=1;
+        *jugad=1;
     }
 }
 
-void run2 (int jug1, int jug2,int jug3, int t1, int t2, int t3, int g, int *x, int *y, int *j){
+void run2 (int jug1, int jug2,int jug3, int t1, int t2, int t3, int g){
     printf("3 jugadores\n");
-    int a x=0,y=0;
+    int a, x=0,y=0;
     char buffer[1024];
     t1 = open(FIFONAME_JUG1, O_RDWR);
     t2 = open(FIFONAME_JUG2, O_RDWR);
     while(1){
         if((a = read(g,buffer, sizeof(buffer))) > 0){
             write(1, buffer, a);
-            *x = buffer[0] - "0";
+            x = buffer[0] - '0';
             close(g);
         }
         
         if((a = read(g,buffer, sizeof(buffer))) > 0){
             write(1, buffer, a);
-            *y = buffer[0] - "0";
+            y = buffer[0] - '0';
             
         }
         if(jug1 == 0){
@@ -343,7 +342,7 @@ void run2 (int jug1, int jug2,int jug3, int t1, int t2, int t3, int g, int *x, i
         }  
     }
 }
-void run3 (int jug1, int jug2,int jug3,int jug3, int t1, int t2, int t3,int t4, int g, int *x, int *y, int *j){
+void run3 (int jug1, int jug2,int jug3,int jug4, int t1, int t2, int t3,int t4, int g){
     printf("3 jugadores\n");
     int a, x=0,y=0;
     char buffer[1024];
@@ -351,12 +350,12 @@ void run3 (int jug1, int jug2,int jug3,int jug3, int t1, int t2, int t3,int t4, 
     while(1){
         if((a = read(g,buffer, sizeof(buffer))) > 0){
             write(1, buffer, a);
-            *x = buffer[0] - "0";
+            x = buffer[0] - '0';
         }
         
         if((a = read(g,buffer, sizeof(buffer))) > 0){
             write(1, buffer, a);
-            *y = buffer[0] - "0";
+            y = buffer[0] - '0';
             
         }
         if(jug1 == 0){
