70bd90a1953a879ef63de9cf2b275b7448f3ea03
zebaten <zebatenx@gmail.com>
2022-01-10 00:32:24 -0300

    juntar clientes

--- cliente1.c
+++ cliente1.c
@@ -14,37 +14,62 @@
 
 #define FIFONAME "YourTooSlow"
 #define FIFONAME_JUG1 "YourTooSlow_j1"
+#define FIFONAME_JUG2 "YourTooSlow_j2"
+#define FIFONAME_JUG3 "YourTooSlow_j3"
+#define FIFONAME_JUG4 "YourTooSlow_j4"
 
 int main(){
-    int a, g, t1;
+    int n, g, t1;
+    int jug=0;
     char buffer[1024];
-    if ((t1 = open(FIFONAME_JUG1, O_RDWR)) < 0)
-    {
-       perror("open T1");
-       exit(1);
-    }
-    if ((g = open(FIFONAME, O_RDWR)) < 0)
-    {
+
+    do{
+        printf ("Indique su numero de jugador ");
+        scanf ("%d", &jug);
+    }while(jug<1 || jug>4);
+    if ((g = open(FIFONAME, O_RDWR)) < 0){
         perror("open FDG");
         exit(1);
     }
-    while(1){
-        //Envía coordenadas al servidor
+    switch(jug){
+        case 1:
+            if ((t1 = open(FIFONAME_JUG1, O_RDWR)) < 0){
+                perror("open C1");
+                exit(1);
+            }
+            break;
+        case 2:
+            if ((t1 = open(FIFONAME_JUG2, O_RDWR)) < 0){
+                perror("open C2");
+                exit(1);
+            }
+            break;
+        case 3:
+            if ((t1 = open(FIFONAME_JUG3, O_RDWR)) < 0){
+                perror("open C3");
+                exit(1);
+            }
+            break;
+        case 4:
+            if ((t1 = open(FIFONAME_JUG4, O_RDWR)) < 0){
+                perror("open C4");
+                exit(1);
+            }
+            break;
+    }
+    while(1){ //Envío de datos al server
         printf("Coordenada x : \n");
-        if ((a = read(0, buffer, sizeof(buffer))) > 0)
-        {
-            write(g, buffer, a);
+        if ((n = read(0, buffer, sizeof(buffer))) > 0){
+            write(g, buffer, n);
         }
 
         printf("Coordenada y : \n");
-        if ((a = read(0, buffer, sizeof(buffer))) > 0)
-        {
-            write(g, buffer, a);
+        if ((n = read(0, buffer, sizeof(buffer))) > 0){
+            write(g, buffer, n);
         }
-
-        if ((a = read(t1, buffer, sizeof(buffer))) > 0)
-        {
-            write(1, buffer, a);
+        sleep(1);
+        if ((n = read(t1, buffer, sizeof(buffer))) > 0){
+            write(1, buffer, n);
         }
+        sleep(1);
     }
-}
---
+++ readme.txt
@@ -0,0 +1,9 @@
+Alejandra Araya
+Sebastián Navarrete
+-----------------------------------------
+
+-La compilación del proyecto debe ser en un ambiente Linux (probado en y funcional en Ubuntu) y en modo SuperUser.
+-En orden de una correcta ejecución, abrir el archivo server, definir la cantidad de jugadores, abrir 
+la cantidad de clientes correspondiente a la cantidad de jugadores para que cada uno maneje e identificar
+en número de jugador correspondiente.
+una consola.
